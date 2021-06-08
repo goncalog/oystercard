c1eec356c80d9e417a8d70dee43fa2f21774675d
IrinaGC <irina.g.cirlan@gmail.com>
2021-06-08 17:01:23 +0100

    ch.7-ch.10

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,16 +1,39 @@
 class Oyster
   
   MAX_BALANCE = 90
-  attr_reader :balance
+  MIN_FARE = 1
+  attr_reader :balance, :in_use
 
   def initialize
     @balance = 0
+    @in_use = false
   end
 
 
   def top_up(money)
-    @balance = @balance + money 
     fail "Oh no! You reached the max balance" if money + balance > MAX_BALANCE
+    @balance = @balance + money
+  end
+
+  
+  def touch_in
+    raise "Ops! Top it up!" if balance < MIN_FARE
+    @in_use = true
+  end
+  
+  def touch_out
+    deduct(MIN_FARE)
+    @in_use = false
+  end
+  
+  def in_journey?
+    @in_use
+  end
+
+  # private
+
+  def deduct(money)
+  @balance -= money
   end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -2,6 +2,7 @@ require "./lib/oyster"
 
 describe Oyster do
 
+  # let(:station) { Station.new }
   it "balance of 0" do
     expect(subject.balance).to eq(0)
   end
@@ -24,5 +25,54 @@ describe Oyster do
     end
   end
 
+  describe "#deduct" do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+    it "deducts money" do
+      subject.top_up(10)
+      expect(subject.deduct(3)).to eq 7
+    end
+  end
+
+  
+  it { is_expected.to respond_to(:touch_out) }
+  it { is_expected.to respond_to(:in_journey?) }
+
+  describe "#touch_in" do
+
+  it { is_expected.to respond_to(:touch_in) }
+  it "touches in" do
+    allow(subject).to receive(:balance).and_return 10
+    expect(subject.touch_in).to eq(true)
+  end
+  it "raises an error if card under £1" do
+  allow(subject).to receive(:balance).and_return 0.5
+  expect{subject.touch_in}.to raise_error "Ops! Top it up!"
+  end
+
+end
+
+  it "touches out" do
+    allow(subject).to receive(:balance).and_return 10
+    expect(subject.touch_out).to eq(false)
+  end
+ 
+  it "check if in journey" do
+    allow(subject).to receive(:balance).and_return 10
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it "check if not in journey" do
+    allow(subject).to receive(:balance).and_return 10
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+
+  it "charges the card when you touch out" do
+    expect{ subject.touch_out }.to change(subject, :balance).by(-1)
+  end
+
+  # expect{subject.top_up(2)}.to change(subject, :balance).by(2)
 
 end
